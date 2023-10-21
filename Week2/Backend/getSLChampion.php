c4871a726499d4667198ed53b4051c1d0125f8a5
GiannisKarampinis <80476980+GiannisKarampinis@users.noreply.github.com>
2023-10-21 22:05:20 +0300

    Add php script for constructing JSON

---
+++ Week2/Backend/getSLChampion.php
@@ -0,0 +1,28 @@
+<?php
+
+$external_data = array();
+$external_data['owner'] = 'Giannis';
+
+$data = array();
+
+$nested_data = array();
+$nested_data['year'] = '2018-2019';
+$nested_data['image'] = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Paok_logo.png/436px-Paok_logo.png';
+$data['paok'] = $nested_data;
+
+$nested_data = array();
+$nested_data['year'] = '2017-2018';
+$nested_data['image'] = 'https://upload.wikimedia.org/wikipedia/el/thumb/3/3b/AEK_Athens_FC_%28logo%29.svg/1200px-AEK_Athens_FC_%28logo%29.svg.png';
+$data['AEK'] = $nested_data;
+
+$nested_data = array();
+$nested_data['year'] = '2019-2020';
+$nested_data['image'] = 'https://upload.wikimedia.org/wikipedia/el/b/b9/OSFP-logo.svg';
+$data['olympiakos'] = $nested_data;
+
+$external_data['data'] = $data;
+
+header("Content-Type: application/json");
+echo json_encode($external_data);
+
+?>
